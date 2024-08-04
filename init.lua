c5fc754993beea2f67f314cf9be929cfa3e561aa
William Stoneham <58570245+RockBacon9922@users.noreply.github.com>
2024-08-04 15:17:20 +0100

    made it so that telescope picks up dot files

--- init.lua
+++ init.lua
@@ -349,11 +349,22 @@ require('lazy').setup({
         --  All the info you're looking for is in `:help telescope.setup()`
         --
         defaults = {
+          file_ignore_patterns = {
+            'node_modules',
+            '.git',
+            'vendor',
+            'build',
+            'dist',
+          },
           --   mappings = {
           --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
           --   },
         },
-        -- pickers = {}
+        pickers = {
+          find_files = {
+            hidden = true,
+          },
+        },
         extensions = {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
